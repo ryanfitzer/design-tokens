b4659db851fc67dfed21bcdd087ba7b77f682cc7
Ryan Fitzer <ryan@ryanfitzer.com>
2021-03-11 09:30:32 -0800

    WIP: Initial layout for color tokens.

--- constants.js
+++ constants.js
@@ -1,31 +1,31 @@
-const path = require( 'path' );
-const pkg = require( './package.json' );
+const path = require('path');
+const pkg = require('./package.json');
 
-const root = path.resolve( __dirname );
+const root = path.resolve(__dirname);
 
-const resolve = ( part ) => path.normalize( `${ path.resolve( root, part ) }/` );
+const resolve = (part) => path.normalize(`${path.resolve(root, part)}/`);
 
 module.exports = {
     namespace: {
         prefix: 'dt',
-        global: 'DT'
+        global: 'DT',
     },
     pkgName: pkg.name,
     paths: {
         root,
         scripts: {
-            root: resolve( 'scripts' ),
+            root: resolve('scripts'),
         },
         src: {
-            root: resolve( 'src' ),
-            tokens: resolve( 'src/tokens' ),
+            root: resolve('src'),
+            tokens: resolve('src/tokens'),
         },
         build: {
-            root: resolve( 'build' ),
-            assets: resolve( 'build/assets' ),
-            styles: resolve( 'build/assets/styles' ),
-            properties: resolve( 'build/properties' ),
-            scripts: resolve( 'build/scripts' ),
-        }
-    }
+            root: resolve('build'),
+            assets: resolve('build/assets'),
+            styles: resolve('build/assets/styles'),
+            properties: resolve('build/properties'),
+            scripts: resolve('build/scripts'),
+        },
+    },
 };
--- scripts/style-dictionary.js
+++ scripts/style-dictionary.js
@@ -1,37 +1,32 @@
-const StyleDictionary = require( 'style-dictionary' );
-const { paths } = require( '../constants' );
+const StyleDictionary = require('style-dictionary');
+const { paths } = require('../constants');
 
+const brands = ['coach', 'kate-spade'];
 
-const brands = [
-  'coach',
-  'kate-spade'
-];
+brands.forEach((brand) => {
+    console.log(`\nBuilding ${brand}`);
 
-brands.forEach(brand => {
-  
-  console.log(`\nBuilding ${brand}`);
-  
-  const styleDictionary = StyleDictionary.extend({
-    include: [
-      `${paths.src.tokens}global/**/*.json`,
-      `${paths.src.tokens}brands/defaults/**/*.json`,
-    ],
-    source: [
-      `${paths.src.tokens}brands/${brand}/**/*.json`
-    ],
-    platforms: {
-      css: {
-        transformGroup: `css`,
-        buildPath: `${paths.build.root}${brand}/`,
-        files: [{
-          destination: `variables.css`,
-          format: `css/variables`
-        }]
-      }
-    }
-  });
-  
-  console.log(JSON.stringify(styleDictionary.properties, null, 2));
+    const styleDictionary = StyleDictionary.extend({
+        include: [
+            `${paths.src.tokens}global/**/*.json`,
+            `${paths.src.tokens}defaults/**/*.json`,
+        ],
+        source: [`${paths.src.tokens}${brand}/**/*.json`],
+        platforms: {
+            css: {
+                transformGroup: `css`,
+                buildPath: `${paths.build.root}${brand}/`,
+                files: [
+                    {
+                        destination: `variables.css`,
+                        format: `css/variables`,
+                    },
+                ],
+            },
+        },
+    });
 
-  styleDictionary.buildAllPlatforms();
+    console.log(JSON.stringify(styleDictionary.properties, null, 2));
+
+    styleDictionary.buildAllPlatforms();
 });
