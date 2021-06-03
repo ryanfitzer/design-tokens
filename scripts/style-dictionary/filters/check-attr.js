315df1610721cdfa6cf0b6b9c242df30d4e69709
Ryan Fitzer <ryanfitzer@users.noreply.github.com>
2021-06-02 17:00:02 -0700

    New: Added `breakpoint` tokens to provide access to `viewport` size.

--- scripts/style-dictionary/filters/check-attr.js
+++ scripts/style-dictionary/filters/check-attr.js
@@ -2,12 +2,16 @@
  * Checks attribute(s) against condition.
  * Can check 1 attribute, or an array of attributes.
  * @param {string|array} attr - The attribute key to check.
- * @param {string} expected - The expected attribute value.
+ * @param {string|function} expected - The expected attribute value, or a function is passed the current attribute value and returns a boolean.
  * @param {boolean} cond - Whether the expected value should match, or not.
  * @returns {boolean}
  * @example
  *      // category === 'color'
  *      checkAttr('category', 'color');
+ *
+ *      // Using a function
+ *      // category === 'color' || category === 'size'
+ *      checkAttr('category', (value) => value === 'color' || value === 'size');
  *
  *      // category !== 'color'
  *      checkAttr('category', 'color', false);
@@ -19,14 +23,19 @@
  *      ]);
  */
 module.exports = (attr, expected, cond = true) => {
+    const transform = (value, expectedValue, cond) => {
+        if (typeof expectedValue === 'function') {
+            return expectedValue(value);
+        } else return (value === expectedValue) === cond;
+    };
+
     return (prop) => {
         if (Array.isArray(attr)) {
-            return attr.every(
-                ([attr, expected, cond = true]) =>
-                    (prop.attributes[attr] === expected) === cond
+            return attr.every(([attr, expected, cond = true]) =>
+                transform(prop.attributes[attr], expected, cond)
             );
         }
 
-        return (prop.attributes[attr] === expected) === cond;
+        return transform(prop.attributes[attr], expected, cond);
     };
 };
